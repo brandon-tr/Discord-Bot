1b0d55103bbaf4764a71b15054b4f123cee5952d
Brandon Travis <travisbrandont@gmail.com>
2017-10-08 17:18:24 -0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,130 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Crawler = require("crawler");
+var owjs = require('overwatch-js');
+
+
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page 
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+        }
+        done();
+    }
+});
+
+const curseWords = ['fuck', 'shit', 'fu-ck', 'nigger', 'nig-ger']
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+ 
+client.on('message', message => {
+    if(message.content.includes('!wow')) {
+        var server = message.content.split(' ')[1];
+        var character = message.content.split(' ')[2]
+        c.queue([{
+            uri: 'https://worldofwarcraft.com/en-us/character/'+server+'/'+character,
+            retries: 0,
+            jQuery: true,
+
+            callback: function (error, res, done) {
+                if(error){
+                    message.reply('Invalid Url')
+                }else if(res.statusCode === 404) {
+                    message.reply('Invalid Page')
+                }else {
+                    var character = new Discord.RichEmbed({image:res.$("a[class='Link Link--text']").attr('href')})
+                    message.reply('Character is  ' + res.$("title").text())
+                    message.reply(res.$("body [class='Link CharacterHeader-ilvl']").text())
+                    message.reply(res.$("body [class='CharacterHeader-detail']").text())
+                    message.reply(character.image)
+                }
+                done();
+            }
+        }]);
+    }
+    if(message.content.includes('!league')) {
+        var character = message.content.split(' ')[1]
+        c.queue([{
+            uri: 'http://na.op.gg/summoner/userName='+character,
+            retries: 0,
+            jQuery: true,
+            jQuery: {
+                name: 'cheerio',
+                options: {
+                    normalizeWhitespace: true,
+                }
+            },
+
+            callback: function (error, res, done) {
+                if(error){
+                    message.reply('Invalid Url')
+                }else if(res.statusCode === 404) {
+                    message.reply('Invalid Page')
+                }else {
+                   message.reply(res.$('title').text())
+                   message.reply('Wins and Losses'+'\n'+res.$("body [class='WinRatioTitle']").text())
+                   var test = res.$("td [class='KDA']").contents().text()
+                   test = test.split('KDA')[3]
+                   message.reply('KDA' + test)
+                   message.reply(res.$("span[class='tierRank']").text())
+                }
+                done();
+            }
+        }]);
+    }
+    if(message.content.includes('!overwatch')) {
+        var playerName = message.content.split(' ')[1]
+        var platform;
+        var region;
+        var name;
+        owjs.search(playerName).then((data) => {
+            for (var index = 0; index < data.length; index++) {
+                platform = data[index].platform
+                region = data[index].region
+                name = data[index].careerLink
+                name = name.split('/')[4]
+            }
+            c.queue([{
+                uri: `https://overwatchtracker.com/profile/${platform}/${region}/${name}`,
+                retries: 0,
+                jQuery: true,
+                jQuery: {
+                    name: 'cheerio',
+                    options: {
+                        normalizeWhitespace: true,
+                        xmlMode: true
+                    }
+                },
+                callback: function (error, res, done) {
+                    if(error){
+                        message.reply('Invalid Url')
+                    }else if(res.statusCode === 404) {
+                        message.reply('Invalid Page')
+                    }else {
+                       message.reply(res.$("div[class='profile-info']").text().trim())
+                       message.reply(res.$("div[class='stats-large']").append('<br>').text().trim())
+                    }
+                    done();
+                }
+            }]);
+        })
+    }
+    for (var index = 0; index < curseWords.length; index++) {
+            if(message.content.includes(curseWords[index])) {
+                message.delete()
+                .then(msg => msg.reply(`That type of language is not allowed`))
+            }
+        }
+    });
+client.on('guildMemberAdd', member => {
+    member.guild.defaultChannel.send(`Welcome ${member}`)
+})
+
+ 
+client.login('');
